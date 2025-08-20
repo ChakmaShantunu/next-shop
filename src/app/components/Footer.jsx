6bbb3fefa79662266ce737f939b9a83ac7df0ffe
shantunu007 <chakmashantunu.web@gmail.com>
2025-08-20 23:41:36 +0600

    footer section done

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            <footer className="bg-amber-300">
-                <h1>awesome next js</h1>
-            </footer>
-        </div>
+        <footer className="footer sm:footer-horizontal bg-[#0F0E0E] text-white rounded-2xl p-10 max-w-11/12 mx-auto">
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+            <form>
+                <h6 className="footer-title">Newsletter</h6>
+                <fieldset className="w-80">
+                    <label>Enter your email address</label>
+                    <div className="join">
+                        <input
+                            type="text"
+                            placeholder="username@site.com"
+                            className="input input-bordered join-item" />
+                        <button className="btn bg-[#EEEEEE]">Subscribe</button>
+                    </div>
+                </fieldset>
+            </form>
+        </footer>
     )
 }
